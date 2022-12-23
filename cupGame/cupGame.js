518b33e6746f1b113afe9ed3848c28740a85e33b
chaya <chay7040@gmail.com>
2022-12-23 15:01:35 +0200

    checking cup game works

--- cupGame/cupGame.js
+++ cupGame/cupGame.js
@@ -9,28 +9,3 @@ function chooseLevel(level){
 function callChooseLevel(){
   var level=prompt("what level do you want?")
 chooseLevel(level);}
-/*
-// var currentUser=JSON.parse(localStorage.getItem(JSON.stringify("currentUser")));
-// var currentUserObj = JSON.parse(currentUser);
-// document.getElementById("enterScore").innerText=currentUserObj.score;
-// var currentScore=currentUserObj.score;
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-  // currentScore++;
-  // //change score
-  // document.getElementById("enterScore").innerText=score;
-  // currentUser.person
-  // localStorage.setItem(currentUserObj.uname,JSON.stringify(currentUser.person));
-  ev.preventDefault();
-
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
-*/
