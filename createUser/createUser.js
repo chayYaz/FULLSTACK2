62f1352f67cb03e02f57df7571f7a4467a43fe09
chaya <chay7040@gmail.com>
2022-12-23 14:44:40 +0200

    checking password is ok before saving

--- createUser/createUser.js
+++ createUser/createUser.js
@@ -7,7 +7,12 @@ const person = {
   score:0,
   password: psw
 }
-if (localStorage.getItem(JSON.stringify(userName)) !== null) {
+//done here as well so wont save bad password
+if(psw.length<8 || psw.length>15){
+  //alert("the password is too long or too short");
+  return;
+}
+if ( localStorage.getItem(JSON.stringify(userName)) !== null) {
   alert("this user name already exists. if it is you please try logging in from login page. if you want to change your passwor, you will have to log in first")
 }
 else{
