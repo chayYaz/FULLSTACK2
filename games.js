0d09baa3cef2967bfd937127098e2a2a487ae473
chaya <chay7040@gmail.com>
2022-12-23 14:49:32 +0200

    fixing current score in game

--- games.js
+++ games.js
@@ -1,9 +1,8 @@
 const games=document.getElementsByClassName("stillCreating");
 
 var currentUser=JSON.parse(localStorage.getItem(JSON.stringify("currentUser")));
-var currentUserObj = JSON.parse(currentUser);
-document.getElementById("enterScore").innerText=currentUserObj.score;
+
+document.getElementById("enterScore").innerText=currentUser.score;
 for(var i=0;i<games.length ;i++){
-  //console.log(games[i]);
   games[i].insertAdjacentHTML("afterend"," <span class='centered'>In progress</span>");
 }
